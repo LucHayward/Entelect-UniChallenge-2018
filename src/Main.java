97ebea9f03f4f53e85af308bc434496753d1b03e
Luc Hayward <luchay10@gmail.com>
2018-08-04 10:06:49 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,7 @@ public class Main {
 
 
 	public static void main(String[] args) {
-		//getInput();
-		map = new int[1000][1000];
-		for (int i = 0; i < 1000; i++) {
-			for (int x = 0; x < 1000; x++) {
-				map[i][x] = i * x;
-			}
-		}
-		System.out.println();
+		getInput();
 	}
 
 	private static void getInput() {
