907bdb441d8ca7a81a9185502f3ff61e5f99467a
Stuart Mesham <stumes3@gmail.com>
2018-08-04 10:24:51 +0200

    Changes

--- src/Location.java
+++ src/Location.java
@@ -1,13 +1,19 @@
 public class Location {
     public String symbol;
     public Coordinate coordinate;
+    public int resources;
     
-    public Location(String symbol, Coordinate coordinate) {
+    public Location(String symbol, Coordinate coordinate, int resources) {
         this.symbol = symbol;
         this.coordinate = coordinate;
+        this.resources = resources;
     }
     
     public boolean isMine() {
         return Character.isUpperCase(symbol.charAt(0));
+    }
+    
+    public boolean isEmpty() {
+        return resources <= 0;
     }
 }
