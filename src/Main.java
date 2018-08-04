1e254256bcb7aff3cc8c0e24c9148fd66864f083
Luc Hayward <luchay10@gmail.com>
2018-08-04 09:50:38 +0200

    added run config

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
 import java.util.List;
 
 public class Main {
-    int[][] map;
-    List<Mine> mines;
+	static int[][] map;
+	List<Mine> mines;
 
-    public static void main(String[] args){
-        getInput();
 
-    }
+	public static void main(String[] args) {
+		//getInput();
+		map = new int[1000][1000];
+		for (int i = 0; i < 1000; i++) {
+			for (int x = 0; x < 1000; x++) {
+				map[i][x] = i * x;
+			}
+		}
+		System.out.println();
+	}
 
-    private static void getInput() {
+	private static void getInput() {
 
-    }
+	}
 
 }
